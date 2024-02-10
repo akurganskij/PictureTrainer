cbbd1d78575cd650a62aa952d00d1016c1467c26
akurganskij <78171489+akurganskij@users.noreply.github.com>
2024-02-10 17:26:06 +0200

    LocalizationBasics

--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PictureTrainer;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+builder.Services.AddLocalization();
+
+
+var host = builder.Build();
+await host.SetDefaultCulture();
+await host.RunAsync();
